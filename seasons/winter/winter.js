14bdad4c4a680d74c7c6fcb01c0c991aa1224c5e
Berk Lee <87292724+LeBerk@users.noreply.github.com>
2022-06-04 14:23:12 +0900

    commit

--- seasons/winter/winter.js
+++ seasons/winter/winter.js
@@ -1,35 +1,130 @@
-const arr1 = ['Coke', 'Sprite', 'Fanta', 'Monster', 'Red Bull', 'Powerade', 'Pokari Sweat']
+// Array for all of the winter shirts 
+const shirts = ['White T-shirt', 'Future T-shirts', 'Orange T-shirt', 'Orange shirt', 'Ferrari T-shirt', 'White T-shirt', 'Black sport T-shirt', 'Octopus T-shirt', 'Blue T-shirt', 'London T-shirt'];
 
-const arr2 = ['Coffee', 'Yuja Tea', 'Black Tea', 'Green Tea', 'Americano', 'Latte'] 
+// Arrays for pants 
+const white_shirt_pants = ['White-Pants', 'Black-Jeans', 'Blue-Jeans', 'Cargo-Pants', 'Autumn-pale-pants'];
 
-const arr3 = ['Ketchup', 'Onion Sauce', 'Black pepper', 'Mustard', 'Soy Sauce', 'Choclate Sauce'] 
+const future_shirt_pants = ['Blue-jeans', 'Black-sports-pants-(green-stripes)', 'Autumn-sport-pants', 'Autumn-green-pants', 'Autumn-pale-pants'];
 
-function random_drink(arr1)
+const orange_t_shirt_pants = ['Black-jeans', 'Blue-jeans', 'Autumn-pale-pants']; 
+
+const orange_shirt_pants = ['Black-jeans', 'Blue-jeans', 'Autumn-pale-pants']; 
+
+const ferrari_shirt_pants = ['Black-jeans', 'Blue-jeans','Cargo-Pants', 'Autumn-sport-pants', 'Autumn-pale-pants'];
+
+const black_sport_shirt_pants = ['Black-jeans', 'Blue-jeans','Cargo-Pants', 'Autumn-sport-pants','Autumn-green-pants'];
+
+const octopus_shirt_pants = ['Black-jeans', 'Blue-jeans','Cargo-Pants','Black-sports-pants-(green-stripes)','Autumn-pale-pants','Autumn-green-pants'];
+
+const blue_shirt_pants = ['Black-jeans', 'Blue-jeans', 'Autumn-pale-pants']; 
+
+const london_shirt_pants = ['Black-jeans', 'Blue-jeans', 'Cargo-Pants', 'Autumn-sport-pants','Autumn-green-pants', 'Autumn-pale-pants'];
+
+
+// Arrays for Jackets 
+
+const jackets = ['Black-Winter-Jacket', 'Red-Winter-Jacket']; 
+
+// Arrays for Hoodies 
+
+// Random Shirt Function 
+
+function random_shirt(shirts)
 {
   
-return arr1[Math.floor(Math.random()*arr1.length)];
+return shirts[Math.floor(Math.random()*shirts.length)];
      
 }
 
-// Divider (So I dont get confused with the code) 
+// Random Pants Function 
 
-function random_drink2(arr2)
-{
-  
-return arr2[Math.floor(Math.random()*arr2.length)];
-     
+function wsp(white_shirt_pants) {
+
+    return white_shirt_pants[Math.floor(Math.random()*white_shirt_pants.length)]; 
+
 }
 
-// 2nd Divider 
+function fsp(future_shirt_pants) {
 
-function random_drink3(arr3)
-{
-  
-return arr3[Math.floor(Math.random()*arr3.length)];
-     
+    return future_shirt_pants[Math.floor(Math.random()*future_shirt_pants.length)]; 
+
 }
 
+function osp(orange_shirt_pants) {
 
-document.getElementById('arr1').innerHTML = random_drink(arr1); 
-document.getElementById('arr1').innerHTML = random_drink(arr2); 
-document.getElementById('arr1').innerHTML = random_drink(arr3); 
+    return orange_shirt_pants[Math.floor(Math.random()*orange_shirt_pants.length)]; 
+
+}
+
+function otsp(orange_t_shirt_pants) {
+
+    return orange_t_shirt_pants[Math.floor(Math.random()*orange_t_shirt_pants.length)]; 
+
+}
+
+function ferrari_sp(ferrari_shirt_pants) {
+
+    return ferrari_shirt_pants[Math.floor(Math.random()*ferrari_shirt_pants.length)]; 
+
+}
+
+function bssp(black_sport_shirt_pants) {
+
+    return black_sport_shirt_pants[Math.floor(Math.random()*black_sport_shirt_pants.length)]; 
+
+}
+
+function octopus_sp(octopus_shirt_pants) {
+
+    return octopus_shirt_pants[Math.floor(Math.random()*octopus_shirt_pants.length)]; 
+
+}
+
+function bsp(blue_shirt_pants) {
+
+    return blue_shirt_pants[Math.floor(Math.random()*blue_shirt_pants.length)];
+
+}
+
+function lsp(london_shirt_pants) {
+
+    return london_shirt_pants[Math.floor(Math.random()*london_shirt_pants.length)];
+
+}
+
+// Random Jacket Functions
+
+function random_jacket(jackets) {
+
+    return jackets[Math.floor(Math.random()*jackets.length)];
+
+}
+
+// Random Hoodie Functions
+
+// Random Outfit Generator (Final Function)
+
+const outfits = () => {
+    switch(random_shirt(shirts)) {
+        case 'White T-shirt': 
+            return 'White-Shirt,  ' + wsp(white_shirt_pants) + ', ' + random_jacket(jackets)
+        case 'Future T-shirts': 
+            return 'Future-T-shirt,  ' + fsp(future_shirt_pants) + ', ' + random_jacket(jackets)
+        case 'Orange T-shirt': 
+            return 'Orange-T-shirt,  ' + osp(orange_t_shirt_pants) + ', ' + random_jacket(jackets)
+        case 'Orange shirt': 
+            return 'Orange-shirt,  ' + osp(orange_shirt_pants) + ', ' + random_jacket(jackets)
+        case 'Ferrari T-shirt': 
+            return 'Ferrari-T-shirt,  ' + ferrari_sp(ferrari_shirt_pants) + ', ' + random_jacket(jackets)
+        case 'Black sport T-shirt':
+            return 'Black-sport-T-shirt,  ' + bssp(black_sport_shirt_pants) + ', ' + random_jacket(jackets)
+        case 'Octopus T-shirt': 
+            return 'Octopus-T-shirt,  ' + octopus_sp(octopus_shirt_pants) + ', ' + random_jacket(jackets)
+        case 'Blue T-shirt': 
+            return 'Blue-T-shirt,  ' + bsp(blue_shirt_pants) + ', ' + random_jacket(jackets)
+        case 'London T-shirt':
+                return 'London-T-shirt,  ' + lsp(london_shirt_pants) + ', ' + random_jacket(jackets)
+    }
+}
+
+document.getElementById('arr1').innerHTML = outfits(); 
